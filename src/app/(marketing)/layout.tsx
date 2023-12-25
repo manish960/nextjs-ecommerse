c72ae0c2d9571e3b1f18d9e4a94900b577e47e66
manish960 <52997259+manish960@users.noreply.github.com>
2023-12-25 22:45:49 +0530

    update

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import '@/app/globals.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className+'h-screen bg-red-100'}>{children}</body>
+      <body className={inter.className+'h-screen'}>{children}</body>
     </html>
   );
 }
